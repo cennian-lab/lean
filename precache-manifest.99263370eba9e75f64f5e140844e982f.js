61a0abbea6ec93a5ddee7c6662a4d29398f0bda3
Ihtesham Ullah <16abelt0709@uetpeshawar.edu.pk>
2020-09-15 15:06:29 +0500

    Updates

---
+++ precache-manifest.99263370eba9e75f64f5e140844e982f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "565b0b8b0a9ee9dbc42245a13b019e2a",
+    "url": "/lean/index.html"
+  },
+  {
+    "revision": "10fa654ffeabba9a7bd5",
+    "url": "/lean/static/js/2.8c4c2294.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/lean/static/js/2.8c4c2294.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a75e9b352f05d4fb76bf",
+    "url": "/lean/static/js/main.a0175246.chunk.js"
+  },
+  {
+    "revision": "af9ba07e873d916fc455",
+    "url": "/lean/static/js/runtime-main.58502897.js"
+  },
+  {
+    "revision": "955d3c2d15acf24591c1c51d70e88f36",
+    "url": "/lean/static/media/profile.955d3c2d.jpg"
+  },
+  {
+    "revision": "55cfb4817b9e054491bd9dafc21087c2",
+    "url": "/lean/static/media/svg-1.55cfb481.svg"
+  },
+  {
+    "revision": "d4846e1163a89864dd49fb03ec6308ed",
+    "url": "/lean/static/media/svg-2.d4846e11.svg"
+  },
+  {
+    "revision": "397c455a49d4f872c292a3dc25432579",
+    "url": "/lean/static/media/svg-3.397c455a.svg"
+  }
+]);
